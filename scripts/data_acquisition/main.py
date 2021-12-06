f5770e2b6e4493968f88f5908b7a86d249f7fe93
deivymg <deivymg@gmail.com>
2021-12-05 21:21:16 -0500

    Update main.py

---
+++ scripts/data_acquisition/main.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+from binance.client import Client
+client = Client(binance_api, binance_secret)
+
+
+#inicializamos las claves
+binance_api='xxxxxxxxxxxxxxxxxxxxxxxxxxx'
+binance_secret='xxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+#solicitamos los datos por minuto iniciando desde los ultimos 15 minutos
+
+timestamp = client._get_earliest_valid_timestamp('BTCUSDT', '15m')
+bars = client.get_historical_klines('BTCUSDT', '1m', timestamp, limit=1000)
+
+#eliminamos atributos no necesarios
+for line in bars:
+    del line[5:]
+    
+#convertimos los datos a un dataframe
+btc_df = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close'])
+btc_df.set_index('date', inplace=True)
+btc_df.to_csv('ultimos 15 minuts.csv')
