8d5124200ab01ed6a3504a938ee5275875cc4aaa
Edwin Perez <edwinaperez@gmail.com>
2021-12-14 22:18:47 +0000

    Cambios realizados de poetry y version de python del proyecto

--- scripts/data_acquisition/main.py
+++ scripts/data_acquisition/main.py
@@ -1,23 +1,42 @@
 import os
 import pandas as pd
 from binance.client import Client
-client = Client(binance_api, binance_secret)
 
 
 #inicializamos las claves
-binance_api='xxxxxxxxxxxxxxxxxxxxxxxxxxx'
-binance_secret='xxxxxxxxxxxxxxxxxxxxxxxxxx'
+binance_api=os.environ.get('binance_api')
+binance_secret=os.environ.get('binance_secret')
+
+
+client = Client(binance_api, binance_secret)
 
 #solicitamos los datos por minuto iniciando desde los ultimos 15 minutos
 
 timestamp = client._get_earliest_valid_timestamp('BTCUSDT', '15m')
-bars = client.get_historical_klines('BTCUSDT', '1m', timestamp, limit=1000)
-
+bars = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE, "15 minutes ago UTC")
 #eliminamos atributos no necesarios
 for line in bars:
     del line[5:]
     
 #convertimos los datos a un dataframe
-btc_df = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close'])
-btc_df.set_index('date', inplace=True)
-btc_df.to_csv('ultimos 15 minuts.csv')
+bars = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE, "15 minutes ago UTC")
+
+btc_df = pd.DataFrame(bars, columns=['timestamp',
+                                    'Open',
+                                    'High',
+                                    'Low',
+                                    'Close',
+                                    'Volume',
+                                    'Close time',
+                                    'Close time',
+                                    'Count',
+                                    'Taker buy base asset volume',
+                                    'Taker buy quote asset volume',
+                                    'Ignore'])
+#eliminamos columnas que no sirven y reordenamos los datos
+btc_df = btc_df[['timestamp','Count', 'Open', 'High', 'Low', 'Close', 'Volume' ]]
+btc_df ['timestamp'] = btc_df['timestamp'].astype(str)
+btc_df ['timestamp'] = btc_df['timestamp'].str[:11]
+btc_df.set_index('timestamp', inplace=True)
+
+btc_df.to_csv('ultimos_15_minutos.csv')
