4a571fd991862580dd7f432b25440ef0829cfc02
Edwin Perez <edwinaperez@gmail.com>
2021-12-16 18:53:49 +0000

    Funcionamiento de preprocessing en scripts/preprocessing/main.py

--- scripts/preprocessing/main.py
+++ scripts/preprocessing/main.py
@@ -7,19 +7,19 @@ def main():
 	data_path = os.path.join(os.environ['BTC_DATAFRAME_PATH'],'supplemental_train.csv')
 	print(data_path)
 	series=pd.read_csv(data_path)
-	print(series.head())
 	series_bitcoin=limpieza(series)
-  	#x_train,y_train,x_test,y_test = preprocessing(series_bitcoin)
-
-	data=preprocessing(series_bitcoin)
-	x_train=data[0]
-	y_train=data[1]
-	x_test=data[2]
-	y_test=data[3]
-	x_train.tofile('x_train')
-	y_train.tofile('y_train')
-	x_test.tofile('x_test')
-	y_test.tofile('y_test')
-  
+	x_train,y_train,x_test,y_test = preprocessing(series_bitcoin)
+	#data = preprocessing(series_bitcoin)
+	#names=["x_train","y_train","x_test","y_test"]
+	
+	save_path = os.path.join(os.environ['PREPROCESS_DATA_PATH'],"preprocessed_data")
+	print(save_path)
+	np.savez(save_path,x_train = x_train,y_train = y_train,x_test = x_test,y_test = y_test)
+		
+	#x_train.tofile('x_train')
+	#y_train.tofile('y_train')
+	#x_test.tofile('x_test')
+	#y_test.tofile('y_test')
+  	
 if __name__=="__main__":
   main()
