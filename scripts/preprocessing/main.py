c6e8180c85ea15d9b64aca4de9018c3d15933ac2
Edwin Perez <edwinaperez@gmail.com>
2021-12-16 21:21:13 +0000

    commit desde el servidor

--- scripts/preprocessing/main.py
+++ scripts/preprocessing/main.py
@@ -3,23 +3,20 @@ import pandas as pd
 import numpy as np
 from packagename.preprocessing.preprocessing import prepararDataset,split_sequences,limpieza,preprocessing
 
+
+#funcion de flujo principal
 def main():
 	data_path = os.path.join(os.environ['BTC_DATAFRAME_PATH'],'supplemental_train.csv')
 	print(data_path)
 	series=pd.read_csv(data_path)
 	series_bitcoin=limpieza(series)
 	x_train,y_train,x_test,y_test = preprocessing(series_bitcoin)
-	#data = preprocessing(series_bitcoin)
-	#names=["x_train","y_train","x_test","y_test"]
-	
+		
 	save_path = os.path.join(os.environ['PREPROCESS_DATA_PATH'],"preprocessed_data")
 	print(save_path)
+	#se almacenan los datos serializados (juntos)
 	np.savez(save_path,x_train = x_train,y_train = y_train,x_test = x_test,y_test = y_test)
 		
-	#x_train.tofile('x_train')
-	#y_train.tofile('y_train')
-	#x_test.tofile('x_test')
-	#y_test.tofile('y_test')
   	
 if __name__=="__main__":
   main()
--- scripts/visualization/dash_app.py
+++ scripts/visualization/dash_app.py
@@ -5,24 +5,30 @@ import plotly.express as px
 import os
 import pandas as pd
 
+"""
+def actualizar_df():
+	make data_acquisition
+"""
 
 def get_df():
-	return pd.read_csv("xxxx")
+	data_path =os.path.join( os.environ['PREPROCESS_DATA_PATH'],"ultimos_15_minutos.csv")
+	df = pd.read_csv(data_path)
+	return df
 
 
 
-external_stylesheets =['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#external_stylesheets =['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets)
+app = dash.Dash(__name__) #, external_stylesheets)
 
 app.layout = html.Div([
 
-html.Button('Get data',id='get-data-button')
-html.Button('Update data',id='update-data-button')
-html.Button('predict data',id='predict-data-button')
-dcc.Graph(id='graph')
+	html.Button('Get data',id='get-data-button'),
+	html.Button('Update data',id='update-data-button'),
+	html.Button('predict data',id='predict-data-button'),
+	dcc.Graph(id='graph')
 
-])
+	])
 
 df = get_df()
 
@@ -40,11 +46,11 @@ def update_info():
 		Input('get-data-button','n_clicks'))
 
 def display_value (value):
-	fig = px.scatter(df,x="x",y="y")
+	fig = px.scatter(df,y="Close")
 	fig.update_layout(transition_duration=500)
 	return fig
 
 
 
 if __name__ == '__main__':
-	app.run_server(debug=True) 
+	app.run_server(host='0.0.0.0', debug=True) 
