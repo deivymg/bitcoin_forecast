ed7aff10c124000a0bf4f6dab87f1e0839417380
Edwin Perez <edwinaperez@gmail.com>
2021-12-16 04:28:04 +0000

    archivos para subir info

---
+++ packagename/datamodels/database.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class DataBasePaths(BaseModel):
+	"""
+
+	Class for database data
+
+	Attributes
+	..........
+	
+	BTC_df : str
+		path to BTC database
+	"""
+	BTC_df : sxtr
+
+
