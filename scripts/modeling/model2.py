c7a9bd1c34aa31bb395958becb8be781594bc025
Edwin Perez <edwinaperez@gmail.com>
2021-12-16 20:01:23 +0000

    carga de modelos ML

--- scripts/modeling/model2.py
+++ scripts/modeling/model2.py
@@ -10,19 +10,27 @@ from keras.layers import LSTM
 from keras.layers import CuDNNLSTM
 
 
-DROPOUT = 0.2
-WINDOW_SIZE = 50
+def main():
 
-ft_model = Sequential()
-ft_model.add(tf.keras.layers.Bidirectional(CuDNNLSTM(WINDOW_SIZE, return_sequences=True),
-  input_shape=(n_steps, n_features) ))
-ft_model.add(tf.keras.layers.Dropout(rate=DROPOUT))
-ft_model.add(tf.keras.layers.Bidirectional(CuDNNLSTM((WINDOW_SIZE * 2), return_sequences=True)))
-ft_model.add(tf.keras.layers.Dropout(rate=DROPOUT))
-ft_model.add(tf.keras.layers.Bidirectional( CuDNNLSTM(WINDOW_SIZE, return_sequences=False)))
-ft_model.add(Dense(units=1))
-ft_model.add(Activation('linear'))
-ft_model.compile(loss="mse", optimizer=tf.optimizers.Adam(learning_rate=1e-3))
+	DROPOUT = 0.2
+	WINDOW_SIZE = 50
 
-ft_model_path = 'model_1_LSTM_2.h5'
-ft_early_stopings = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='min')
+	ft_model = Sequential()
+	ft_model.add(tf.keras.layers.Bidirectional(CuDNNLSTM(WINDOW_SIZE, return_sequences=True),
+	input_shape=(n_steps, n_features) ))
+	ft_model.add(tf.keras.layers.Dropout(rate=DROPOUT))
+	ft_model.add(tf.keras.layers.Bidirectional(CuDNNLSTM((WINDOW_SIZE * 2), return_sequences=True)))
+	ft_model.add(tf.keras.layers.Dropout(rate=DROPOUT))
+	ft_model.add(tf.keras.layers.Bidirectional( CuDNNLSTM(WINDOW_SIZE, return_sequences=False)))
+	ft_model.add(Dense(units=1))
+	ft_model.add(Activation('linear'))
+	ft_model.compile(loss="mse", optimizer=tf.optimizers.Adam(learning_rate=1e-3))
+
+	ft_model_path = 'model_1_LSTM_2.h5'
+	ft_early_stopings = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='min')
+
+
+if __name__=="__main__":
+  main()
+
+
