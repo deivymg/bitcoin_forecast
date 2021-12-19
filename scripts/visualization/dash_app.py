5f9cd07a5de578cff520c872fc2ed8a07f2a895d
Edwin Perez <edwinaperez@gmail.com>
2021-12-19 23:54:49 +0000

    cambio al dashboard

--- scripts/visualization/dash_app.py
+++ scripts/visualization/dash_app.py
@@ -12,45 +12,81 @@ def actualizar_df():
 """
 
 def get_df():
-	data_path =os.path.join( os.environ['PREPROCESS_DATA_PATH'],"ultimos_15_minutos.csv")
+	data_path =os.path.join( os.environ['BINANCE_RAW_PATH'],"ultimos_15_minutos.csv")
 	df = pd.read_csv(data_path)
 	return df
-
-
-
 
 
 app = dash.Dash(__name__) #, external_stylesheets)
 
 app.layout = html.Div([
+    html.H1('BITCOIN FORECAST'),
+    dcc.Checklist(
+        id='toggle-rangeslider',
+        options=[{'label': 'Include Rangeslider', 
+                  'value': 'slider'}],
+        value=['slider']
+    ),
+    dcc.Graph(id="graph"),
+    html.Br(),
+    html.Div(id='body-div'),
+    html.Br(),
+    html.Div([
+    html.Div([
+    html.Button('Update data', id='update-data-button'),
+    ]),
+    html.Div([
+    html.Button('predict data', id='predict-data-button'),
+    ]),
+    ])
 
-	html.Button('Get data',id='get-data-button'),
-	html.Button('Update data',id='update-data-button'),
-	html.Button('predict data',id='predict-data-button'),
-	dcc.Graph(id='graph')
+])
 
-	])
+
 
 df = get_df()
 
+def Candlestick_fig(df: DataFrame):
+  fig = go.Figure(data=[go.Candlestick(x=df['timestamp'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
+  fig 
+
+  return fig
 
 
+@app.callback(
+    Output("graph", "figure"), 
+    [Input("toggle-rangeslider", "value")])
 
-@app.callback(Output('graph','figure'),
-		Input('update-data-button','n_clicks'))
+def display_candlestick(value):
+    
+    fig = Candlestick_fig(df)
+    fig.update_layout(
+        xaxis_rangeslider_visible='slider' in value
+    )
 
-def update_info():
-	return get_df()
+    return fig    
 
 
-@app.callback(Output('graph','figure'),
-		Input('get-data-button','n_clicks'))
+@app.callback(
+    Output("body-div", "children"),
+    Input("predict-data-button", "n_clicks")
+)
+def change_text(n_clicks):
+      """
+      funcion para predecir 
 
-def display_value (value):
-	fig = px.scatter(df,y="Close")
-	fig.update_layout(transition_duration=500)
-	return fig
-
+      """
+      if n_clicks is None:
+        raise PreventUpdate
+      else:
+        y_pred=- -0.00566196
+        datos = f" el modelo predice : {y_pred}"
+        return datos
+      
 
 
 if __name__ == '__main__':
